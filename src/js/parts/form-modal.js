563f41bd1335d809c784264c8cf9f72e71acde79
codersuperpro <coder.super.pro@gmail.com>
2019-05-28 15:51:18 +0500

    First commit

--- src/js/parts/form-modal.js
+++ src/js/parts/form-modal.js
@@ -4,59 +4,51 @@ const formModal=()=>{
         success: 'Спасибо! Скоро мы с вами свяжемся!',
         failure: 'Что-то пошло не так...'
     };
+    let form = document.querySelectorAll('.form'),
+        input = document.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+        statusMessage.classList.add('status');
 
-    // let form = document.querySelectorAll('.form'),
-    //     input = document.getElementsByTagName('input'),
-        
-    //     statusMessage = document.createElement('div');
-    //     statusMessage.classList.add('status');
+    form.forEach(form=> form.addEventListener('submit', function(e){
+        e.preventDefault();
+        form.appendChild(statusMessage);
+        let formData = new FormData(form);
+            let obj = {};
+            formData.forEach((value,key)=> {
+                obj[key] = value;
+            });
+            let json = JSON.stringify(obj);
 
-    function sendForm(elem) {
-        elem.addEventListener('submit', function(e) {
-            e.preventDefault();
-            elem.appendChild(statusMessage);
-            let formData = new FormData(elem);
-                let obj = {};
-                formData.forEach((value,key)=> {
-                    obj[key] = value;
-                });
-                let json = JSON.stringify(obj);
-
-                function postData() {
-                    return new Promise((resolve, reject)=> {
-                        let request = new XMLHttpRequest();
-                        request.open('POST', 'server.php');
-                        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-
-                        request.onreadystatechange = function() {
-                            if (request.readyState < 4) {
-                                resolve()
-                            } else if(request.readyState === 4) {
-                                if (request.status == 200 && request.status < 3){
-                                    resolve()
-                                }
-                                else {
-                                    reject()
-                                }
-                            }
-                        };
-                        request.send(json);        
-                    });
-                }
-                const clearInput=()=> {
-                    for (let i = 0; i < input.length; i++) {
-                        input[i].value = '';
+        function postData(data) {
+            return new Promise((resolve, reject)=> {
+                let request = new XMLHttpRequest();
+                request.open('POST', 'server.php');
+                request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+                request.onreadystatechange = ()=> {
+                    if (request.readyState < 4) {
+                        resolve()
+                    } else if(request.readyState === 4) {
+                        if (request.status == 200 && request.status < 3){
+                            resolve()
+                        } else {
+                            reject()
+                        }
                     }
-                }
-
-                postData(formData)
-                    .then(()=> statusMessage.innerHTML = message.loading)
-                    .then(()=> statusMessage.innerHTML = message.success)
-                    .catch(()=> statusMessage.innerHTML = message.failure)
-                    .then(clearInput)
-    });
-    }
-    // sendForm(form);
+                };
+                request.send(json);        
+            });
+        }
+        const clearInput=()=> {
+            for (let i = 0; i < input.length; i++) {
+                input[i].value = '';
+            }
+        }
+        postData(formData)
+            .then(()=> statusMessage.innerHTML = message.loading)
+            .then(()=> statusMessage.innerHTML = message.success)
+            .catch(()=> statusMessage.innerHTML = message.failure)
+            .then(clearInput)
+    }));
 
     $('.phone_form').keydown(function(){
         $(this).val($(this).val().replace(/[^\d]|'+'/g, '').slice(0, 11));
